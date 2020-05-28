47db95c876dba83912502b189d791167fedb75c0
devsheikh3 <devsheikh3@gmail.com>
2020-05-28 05:36:41 +0500

    Updated

--- app/src/main/java/com/demit/mehraan/Mobile_Verification.java
+++ app/src/main/java/com/demit/mehraan/Mobile_Verification.java
@@ -9,20 +9,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class Mobile_Verification extends Fragment {
 
     Button verifynumber;
+    ImageView close, back;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View view=inflater.inflate(R.layout.fragment_mobile__verification, container, false);
-
-       //fuck you there
-
+       close=(ImageView)view.findViewById(R.id.closemblverifybtnid);
+       back=(ImageView)view.findViewById(R.id.backmblverifybtnid);
        verifynumber=(Button)view.findViewById(R.id.verifynumberid);
+
+       close.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+              Intent intent= new Intent(getContext(),Dashboard.class);
+              startActivity(intent);
+
+           }
+       });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(getContext(),Dashboard.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
         verifynumber.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
--- app/src/main/java/com/demit/mehraan/PinCode.java
+++ app/src/main/java/com/demit/mehraan/PinCode.java
@@ -1,5 +1,6 @@
 package com.demit.mehraan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class PinCode extends Fragment {
+
+    Button resendpin;
+    ImageView close, back;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       View view=inflater.inflate(R.layout.fragment_pin_code, container, false);
+
+        close=(ImageView)view.findViewById(R.id.closepinverifybtnid);
+        back=(ImageView)view.findViewById(R.id.backpinverifybtnid);
+        resendpin=(Button)view.findViewById(R.id.resendpinid);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(getContext(),Dashboard.class);
+                startActivity(intent);
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getFragmentManager().beginTransaction().replace(R.id.signupfragid,new Mobile_Verification()).commit();
+
+            }
+        });
+
+
+
+        resendpin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
 
 
 
